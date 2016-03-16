bcd37e58c7fd628c6feda0fc1815cf2f0d1980f2
christeh <hansen.christer@gmail.com>
2016-03-16 14:17:18 +0100

    Initial commit

---
+++ app/src/main/java/no/hinesna/locationsamling/MainActivity.java
@@ -0,0 +1,64 @@
+package no.hinesna.locationsamling;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.config.LocationParams;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Log.d(TAG, SmartLocation.with(this).location().state().isGpsAvailable()+"");
+
+        SmartLocation.with(this).location()
+                .config(LocationParams.NAVIGATION)
+                .start(new OnLocationUpdatedListener() {
+                    @Override
+                    public void onLocationUpdated(Location location) {
+                        TextView lat = (TextView)findViewById(R.id.latitude);
+                        TextView lng = (TextView)findViewById(R.id.longitude);
+                        lat.setText(location.getLatitude()+"");
+                        lng.setText(location.getLongitude()+"");
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
